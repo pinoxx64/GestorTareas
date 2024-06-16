01dc9adb425419b8cb8d7ec3fae37bd2503c8b40
Raul GM <loscanos3raul@gmail.com>
2024-06-16 18:40:15 +0200

    intento de arreglo de las rutas y creacion de parte del frontend centrado en los usuarios

--- backend/gestorTareas/routes/api.php
+++ backend/gestorTareas/routes/api.php
@@ -5,9 +5,20 @@ use App\Http\Controllers\TareaController;
 use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('usuario', [UsuarioController::class], 'usuariosGet');
+Route::post('usuario', [UsuarioController::class], 'usuarioPost');
+Route::get('usuario/{id}', [UsuarioController::class], 'usuarioGet');
+Route::put('usuario/{id}', [UsuarioController::class], 'usuarioPut');
+Route::delete('usuario/{id}', [UsuarioController::class], 'usuarioDelete');
 
+Route::get('tarea', [tareaController::class], 'tareasGet');
+Route::post('tarea', [tareaController::class], 'tareaPost');
+Route::get('tarea/{id}', [tareaController::class], 'tareaGet');
+Route::put('tarea/{id}', [tareaController::class], 'tareaPut');
+Route::delete('tarea/{id}', [tareaController::class], 'tareaDelete');
 
-
-
-
-
+Route::get('rol', [rolController::class], 'rolesGet');
+Route::post('rol', [rolController::class], 'rolPost');
+Route::get('rol/{id}', [rolController::class], 'rolGet');
+Route::put('rol/{id}', [rolController::class], 'rolPut');
+Route::delete('rol/{id}', [rolController::class], 'rolDelete');
---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    baseUrl:'http://localhost:9090/api',
+    urlSocket:'http://localhost:8090',
+    urlUsuario:'/usuario',
+    urlRol:'/rol',
+    urlUsuarioRol:'/usuariorol',
+    urlTarea:'/tarea',
+    urlAuth:'/auth',
+    urlLogin:'/login',
+    urlRegistro:'/register'
+};
