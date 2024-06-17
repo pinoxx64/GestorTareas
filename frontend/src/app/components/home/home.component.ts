28977ca1ff7704dbe7f1fa99423febf60165042c
Raul GM <loscanos3raul@gmail.com>
2024-06-17 15:14:15 +0200

    avances con el home y el login

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
-import { EventoComponent } from '../evento/evento.component';
-import { VerUsuarioComponent } from '../ver-usuario/ver-usuario.component';
 import { RegistroComponent } from '../registro/registro.component';
-import { InicioComponent } from '../inicio/inicio.component';
 import { LoginComponent } from '../login/login.component';
 
 @Component({
@@ -18,11 +15,8 @@ import { LoginComponent } from '../login/login.component';
     RouterOutlet,
     RouterLink,
     CommonModule,
-    EventoComponent,
-    VerUsuarioComponent,
     RegistroComponent,
-    LoginComponent,
-    InicioComponent
+    LoginComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
